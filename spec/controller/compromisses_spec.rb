0a9806bc8ae27c0f993839490fc03c0cd24f4007
Leandro <ldesouzarodrigues@yahoo.com.br>
2015-08-04 16:20:34 -0300

    implementacao parcial de teste de  controller

---
+++ spec/controller/compromisses_spec.rb
@@ -0,0 +1,9 @@
+# require 'rails_helper'
+
+# RSpec.describe CompromisseController, type: :controller do
+#   context "index" do
+#     it "show the last reminders" do
+#       expect()
+#     end
+#   end
+# end
